15c9554cbf7b1ff1e33e6b76de462bd04ad47c81
刘建阳 <2022010733@admin.secoder.net>
2024-07-26 22:37:24 +0800

    price entry

--- src/services/transportService.ts
+++ src/services/transportService.ts
@@ -129,9 +129,9 @@ export const searchTransportDetails = (
   ) => {
     const queryParams = new URLSearchParams();
   
-    if (params.startsite_id !== undefined) queryParams.append('startsite_id', params.startsite_id.toString());
-    if (params.endsite_id !== undefined) queryParams.append('endsite_id', params.endsite_id.toString());
-    if (params.goods_id !== undefined) queryParams.append('goods_id', params.goods_id.toString());
+    if (params.startsite_id !== 0) queryParams.append('startsite_id', params.startsite_id.toString());
+    if (params.endsite_id !== 0) queryParams.append('endsite_id', params.endsite_id.toString());
+    if (params.goods_id !== 0) queryParams.append('goods_id', params.goods_id.toString());
     if (params.start_date !== null) queryParams.append('start_date', params.start_date);
     if (params.end_date !== null) queryParams.append('end_date', params.end_date);
   
