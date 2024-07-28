9e98c10d92566684271dbbf4aab50648950656f8
刘建阳 <2022010733@admin.secoder.net>
2024-07-29 07:00:22 +0800

    end entry

--- src/components/reports/SiteEntryM.vue
+++ src/components/reports/SiteEntryM.vue
@@ -66,13 +66,15 @@
   
           const reader = new FileReader();
           reader.onload = (e) => {
-            const data = new Uint8Array(e.target.result as ArrayBuffer);
-            const workbook = XLSX.read(data, { type: 'array' });
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
-            const htmlContent = XLSX.utils.sheet_to_html(worksheet);
-            excelContent.value = styleExcelTable(htmlContent);
-            tableVisible.value = true;
+            if (e.target) {
+        const data = new Uint8Array(e.target.result as ArrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const htmlContent = XLSX.utils.sheet_to_html(worksheet);
+        excelContent.value = styleExcelTable(htmlContent);
+        tableVisible.value = true;
+      }
           };
           reader.readAsArrayBuffer(blob);
         } catch (error) {
