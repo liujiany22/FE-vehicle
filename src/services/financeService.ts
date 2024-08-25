1d6433356ec949406394870e52956613d32d63c2
刘建阳 <2022010733@admin.secoder.net>
2024-08-25 18:23:21 +0800

    修复settlement

--- src/services/financeService.ts
+++ src/services/financeService.ts
@@ -18,6 +18,7 @@ export const getSettlement = (
     params: {
         ownerName?: string;
         project_id?: number;
+        goods_id?: number;
         startsite_id?: number;
         endsite_id?: number;
         start_date?: string | null;
@@ -28,6 +29,7 @@ export const getSettlement = (
 
     if (params.ownerName && params.ownerName !== '') queryParams.append('ownerName', params.ownerName.toString());
     if (params.project_id && params.project_id !== 0) queryParams.append('project_id', params.project_id.toString());
+    if (params.goods_id && params.goods_id !== 0) queryParams.append('goods_id', params.goods_id.toString());
     if (params.startsite_id && params.startsite_id !== 0) queryParams.append('startsite_id', params.startsite_id.toString());
     if (params.endsite_id && params.endsite_id !== 0) queryParams.append('endsite_id', params.endsite_id.toString());
     if (params.start_date) queryParams.append('start_date', params.start_date);
